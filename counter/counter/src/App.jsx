784c5fbe5b1d35fc1f92e1c524f4ab915c5f517b
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2025-02-28 21:40:11 +0545

    simple counter

---
+++ counter/counter/src/App.jsx
@@ -0,0 +1,12 @@
+import React ,{useState} from "react";
+function App(){
+  const [counter,setCounter]= useState(0);
+
+  return (<div>
+      <h1>Counter:{counter}</h1>
+      <button onClick={()=> setCounter(counter+1)}>Increase</button>
+<button onClick={()=> counter>0 && setCounter(counter-1)}>Decrease</button>
+
+  </div>)
+}
+export default App
